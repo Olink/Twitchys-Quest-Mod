361fc9e58691792eb2277043e496baeb20416915
Zack Piispanen <olink@shankshock.com>
2012-03-13 12:15:25 -0400

    Initial commit

---
+++ QMain.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Terraria;
+using MySql.Data.MySqlClient;
+using Hooks;
+using TShockAPI;
+using TShockAPI.DB;
+using System.ComponentModel;
+using LuaInterface;
+using System.IO;
+
+namespace QuestSystemLUA
+{
+    [APIVersion(1, 11)]
+    public class QMain : TerrariaPlugin
+    {        
+        public override string Name
+        {
+            get { return "QuestPluginLUA"; }
+        }
+        public override string Author
+        {
+            get { return "Created by Twitchy."; }
+        }
+        public override string Description
+        {
+            get { return ""; }
+        }
+        public override Version Version
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Version; }
+        }
+        public static List<QPlayer> Players = new List<QPlayer>();
+        public static List<Quest> QuestPool = new List<Quest>();
+        public static List<StoredQPlayer> StoredPlayers = new List<StoredQPlayer>();
+        public static List<QuestRegion> QuestRegions = new List<QuestRegion>();
+        public static SqlTableEditor SQLEditor;
+        public static SqlTableCreator SQLWriter;
+
+        public override void Initialize()
+        {
+            TypesList.SetupTyps();
+
+            NetHooks.GreetPlayer += OnGreetPlayer;
+            ServerHooks.Leave += OnLeave;
+            NetHooks.GetData += GetData;
+            GameHooks.Initialize += OnInitialize;
+            GameHooks.Update += OnUpdate;
+            ServerHooks.Chat += OnChat;
+
+            GetDataHandlers.InitGetDataHandler();     
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                NetHooks.GreetPlayer -= OnGreetPlayer;
+                ServerHooks.Leave -= OnLeave;
+                NetHooks.GetData -= GetData;
+                GameHooks.Initialize -= OnInitialize;
+                GameHooks.Update -= OnUpdate;
+                ServerHooks.Chat -= OnChat;
+            }
+            base.Dispose(disposing);
+        }
+        public void OnInitialize()
+        {
+            Main.ignoreErrors = true;
+            Main.rand = new Random();
+
+            SQLEditor = new SqlTableEditor(TShock.DB, TShock.DB.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+            SQLWriter = new SqlTableCreator(TShock.DB, TShock.DB.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+
+            Commands.ChatCommands.Add(new Command(QCommands.GetCoords, "getcoords"));
+            Commands.ChatCommands.Add(new Command(QCommands.HitCoords, "hitcoords"));
+            Commands.ChatCommands.Add(new Command("usequest", QCommands.ListQuest, "listquests"));
+            Commands.ChatCommands.Add(new Command("usequest", QCommands.StartQuest, "startquest"));
+            Commands.ChatCommands.Add(new Command("questregion", QCommands.QuestRegion, "questr"));
+            Commands.ChatCommands.Add(new Command("reloadqdata", QCommands.LoadQuestData, "reloadquestdata"));
+            Commands.ChatCommands.Add(new Command("giveq", QCommands.GiveQuest, "giveq"));
+            Commands.ChatCommands.Add(new Command("stopquest", QCommands.StopQuest, "stopquest")); 
+            
+            var table = new SqlTable("QuestPlayers",
+                 new SqlColumn("LogInName", MySqlDbType.Text) { Unique = true },
+                 new SqlColumn("QuestPlayerData", MySqlDbType.Text)
+             );
+            SQLWriter.EnsureExists(table);
+
+            table = new SqlTable("QuestRegions",
+                new SqlColumn("RegionName", MySqlDbType.Text) { Unique = true },
+                new SqlColumn("X1", MySqlDbType.Int32),
+                new SqlColumn("Y1", MySqlDbType.Int32),
+                new SqlColumn("X2", MySqlDbType.Int32),
+                new SqlColumn("Y2", MySqlDbType.Int32),
+                new SqlColumn("Quests", MySqlDbType.Text),
+                new SqlColumn("EntryMessage", MySqlDbType.Text),
+                new SqlColumn("ExitMessage", MySqlDbType.Text)
+            );
+            SQLWriter.EnsureExists(table);
+
+            QTools.LoadQuestData();
+        }
+        public QMain(Main game)
+            : base(game)
+        {
+            Order = -10;
+        }
+        public void OnChat(messageBuffer msg, int ply, string text, HandledEventArgs e)
+        {
+            if (e.Handled)
+                return;
+
+            var player = QTools.GetPlayerByID(ply);
+            if (player.AwaitingChat)
+            {
+                player.LastChatMessage = text;
+                player.AwaitingChat = false;
+
+                if (player.HideChat)
+                    e.Handled = true;
+            }            
+        }
+        public void OnUpdate()
+        {
+            foreach (QPlayer player in Players)
+            {
+                if (!player.IsLoggedIn && player.TSPlayer.IsLoggedIn)
+                {
+                    player.MyDBPlayer = QTools.GetStoredPlayerByIdentification(player);
+
+                    if (player.MyDBPlayer == null)
+                    {
+                        StoredQPlayer splayer = new StoredQPlayer(player.TSPlayer.UserAccountName, new List<QuestPlayerData>());
+                        StoredPlayers.Add(splayer);
+                        player.MyDBPlayer = splayer;
+                        QTools.UpdateStoredPlayersInDB();
+                    }
+
+                    player.IsLoggedIn = true;
+                }
+
+                if (player.LastTilePos != new Vector2(player.TSPlayer.TileX, player.TSPlayer.TileY))
+                {
+                    bool inhouse = false;
+                    foreach (QuestRegion qr in QuestRegions)
+                    {
+                        if (qr.Area.Intersects(new Rectangle(player.TSPlayer.TileX, player.TSPlayer.TileY, 1, 1)))
+                        {
+                            if (player.CurQuestRegion != qr.Name)
+                            {
+                                player.CurQuestRegion = qr.Name;
+                                player.InHouse = true;
+
+                                if (qr.MessageOnEntry != "")
+                                    player.TSPlayer.SendMessage(qr.MessageOnEntry, Color.Magenta);
+                            }
+                            inhouse = true;
+                        }
+                        if (!inhouse && player.InHouse)
+                        {
+                            if (qr.MessageOnExit != "")
+                                player.TSPlayer.SendMessage(qr.MessageOnExit, Color.Magenta);
+                            player.CurQuestRegion = "";
+                            player.InHouse = false;
+                        }
+
+                        player.LastTilePos = new Vector2(player.TSPlayer.TileX, player.TSPlayer.TileY);
+                    }
+                }
+            }
+        }
+        public void OnGreetPlayer(int who, HandledEventArgs e)
+        {
+            QPlayer player = new QPlayer(who);
+
+            lock (Players)
+                Players.Add(player);
+        }
+        public void OnLeave(int ply)
+        {
+            lock (Players)
+            {
+                for (int i = 0; i < Players.Count; i++)
+                {
+                    if (Players[i].Index == ply)
+                    {
+                        Players.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+        }
+        private void GetData(GetDataEventArgs e)
+        {
+            PacketTypes type = e.MsgID;
+            var player = TShock.Players[e.Msg.whoAmI];
+
+            if (player == null)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!player.ConnectionAlive)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            using (var data = new MemoryStream(e.Msg.readBuffer, e.Index, e.Length))
+            {
+                try
+                {
+                    if (GetDataHandlers.HandlerGetData(type, player, data))
+                        e.Handled = true;
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ TypesList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuestSystemLUA
+{
+    public class TypesList
+    {
+        public static Dictionary<string, byte> tileTypeNames = new Dictionary<string, byte>();
+        public static Dictionary<string, byte> wallTypeNames = new Dictionary<string, byte>();
+
+        public static void SetupTyps()
+        {
+            tileTypeNames.Add("dirt", 0);
+            tileTypeNames.Add("stone", 1);
+            tileTypeNames.Add("grass", 2);
+            tileTypeNames.Add("iron ore", 6);
+            tileTypeNames.Add("copper ore", 7);
+            tileTypeNames.Add("gold ore", 8);
+            tileTypeNames.Add("silver ore", 9);
+            tileTypeNames.Add("wooden platform", 19);
+            tileTypeNames.Add("demonite ore", 22);
+            tileTypeNames.Add("corrupted grass", 23);
+            tileTypeNames.Add("ebonstone", 25);
+            tileTypeNames.Add("wood", 30);
+            tileTypeNames.Add("corruption thorn", 32);
+            tileTypeNames.Add("meteorite", 37);
+            tileTypeNames.Add("gray brick", 38);
+            tileTypeNames.Add("clay brick", 39);
+            tileTypeNames.Add("clay", 40);
+            tileTypeNames.Add("blue brick", 41);
+            tileTypeNames.Add("green brick", 43);
+            tileTypeNames.Add("pink brick", 44);
+            tileTypeNames.Add("gold brick", 45);
+            tileTypeNames.Add("silver brick", 46);
+            tileTypeNames.Add("copper brick", 47);
+            tileTypeNames.Add("spike", 48);
+            tileTypeNames.Add("cobweb", 51);
+            tileTypeNames.Add("sand", 53);
+            tileTypeNames.Add("glass", 54);
+            tileTypeNames.Add("obsidian", 56);
+            tileTypeNames.Add("ash", 57);
+            tileTypeNames.Add("hellstone", 58);
+            tileTypeNames.Add("mud", 59);
+            tileTypeNames.Add("jungle grass", 60);
+            tileTypeNames.Add("sapphire", 63);
+            tileTypeNames.Add("ruby", 64);
+            tileTypeNames.Add("emerald", 65);
+            tileTypeNames.Add("topaz", 66);
+            tileTypeNames.Add("amethyst", 67);
+            tileTypeNames.Add("diamond", 68);
+            tileTypeNames.Add("mushroom grass", 70);
+            tileTypeNames.Add("obsidian brick", 75);
+            tileTypeNames.Add("hellstone brick", 76);
+            tileTypeNames.Add("water", 253);
+            tileTypeNames.Add("lava", 254);
+            wallTypeNames.Add("stone wall", 1);
+            wallTypeNames.Add("dirt wall untakeable", 2);
+            wallTypeNames.Add("ebonstone wall", 3);
+            wallTypeNames.Add("wood wall", 4);
+            wallTypeNames.Add("gray brick wall", 5);
+            wallTypeNames.Add("red brick wall", 6);
+            wallTypeNames.Add("blue brick wall", 7);
+            wallTypeNames.Add("green brick wall", 8);
+            wallTypeNames.Add("pink brick wall", 9);
+            wallTypeNames.Add("gold brick wall", 10);
+            wallTypeNames.Add("silver brick wall", 11);
+            wallTypeNames.Add("copper brick wall", 12);
+            wallTypeNames.Add("hellstone brick wall", 13);
+            wallTypeNames.Add("obsidian brick wall", 14);
+            wallTypeNames.Add("mud wall", 15);
+            wallTypeNames.Add("dirt wall", 16);
+        }
+    }
+}
